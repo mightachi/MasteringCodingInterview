3e47787c9e6ab10a69ae91b4bcf4edb3f4fe4f17
sukh-sagar-shukla <sss.sukhsagar@gmail.com>
2025-10-13 20:58:48 +0530

    [Practice] Numpy and Pandas

---
+++ real_interview/find_revenue_by_region.py
@@ -0,0 +1,8 @@
+data = {
+    "orderid": [1, 2, 3, 4, 5],
+    "customerid": [1, 2, 1, 3, 2],
+    "region": ["North", "South", "North", "East", "South"],
+    "unitprice": [10, 20, 10, 30, 20],
+    "quantity": [1, 2, 3, 1, 1]
+    "discount": [0.1, 0.2, 0.1, 0.3, 0.2]
+}
